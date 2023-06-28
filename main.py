737ccabbefc1948404c3008a02e43868cf04e93c
Thipekesh28 <80377529+Thipekesh28@users.noreply.github.com>
2023-06-28 11:11:02 +0000

    nanner after

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id':1,
+    'title':'Firwall'
+     
+  },
+ {
+    'id':2,
+    'title':'stop'
+     
+  },
+   {
+    'id':3,
+    'title':'share'
+     
+  },
+  
+
+]
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('home.html', jobs=JOBS) #passing home.html here 
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  #second url addese
+  
 
 
 if __name__ == "__main__":
